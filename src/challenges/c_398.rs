0984ac1edd1b1aa6e8eb152a95ecb5c9c374850f
Jared Nagle <jared@worldstack.net>
2022-06-05 21:05:41 +1000

    WIP

---
+++ src/challenges/c_398.rs
@@ -0,0 +1,11 @@
+fn matrix_sum(matrix: &[[u32]]) -> Option<u32> {
+
+}
+
+fn load_matrix(filename: &str) -> [[u32]] {
+
+}
+
+mod tests {
+    use super::*;
+}
---
+++ src/challenges/c_399.rs
@@ -0,0 +1,41 @@
+fn calculate_sum(phrase: &str) -> Option<u32> {
+    phrase.bytes()
+        .map(|x| (x - 96) as u32)
+        .fold(Option::from(0),
+              |acc, y| acc.map(|sum| sum + y))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::challenge::challenge_399::calculate_sum;
+
+    #[test]
+    fn test_0() {
+        assert_eq!(calculate_sum(""), Some(0));
+    }
+
+    #[test]
+    fn test_1() {
+        assert_eq!(calculate_sum("a"), Some(1));
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(calculate_sum("z"), Some(26));
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(calculate_sum("cab"), Some(6));
+    }
+
+    #[test]
+    fn test_4() {
+        assert_eq!(calculate_sum("excellent"), Some(100));
+    }
+
+    #[test]
+    fn test_5() {
+        assert_eq!(calculate_sum("microspectrophotometries"), Some(317));
+    }
+}
